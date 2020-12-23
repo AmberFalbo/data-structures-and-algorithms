5c9e50e54192231339023adb0a01326fd88050a4
Amber Falbo <amberannfalbo@gmail.com>
2020-12-22 23:24:21 -0800

    LL iterations

---
+++ python/challenges/linked_list/README.md
@@ -0,0 +1,18 @@
+# LL Itereration 12.22.2020 
+
+## Challenge
+.append(value) which adds a new node with the given value to the end of the list
+.insertBefore(value, newVal) which add a new node with the given newValue immediately before the first value node
+.insertAfter(value, newVal) which add a new node with the given newValue immediately after the first value node
+
+## Approach & Efficiency
+Mob programming using notes and researching together
+
+## Solution
+![LL_Iterations](../assets/ll_insertions.jpg)
+
+
+Resources:
+- Mob Programming
+
+Worked with Anthony Beaver, Audrena Vairca, Kim Damalas and Robert Carter. 
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,42 @@
-from linked_list.linked_list import LinkedList
+from linked_list import LinkedList, Node
+import pytest
 
 
 def test_import():
     assert LinkedList
+    assert Node
+
+# add a node to the end of the linked list
+def test_append():
+    node = Node(0)
+    link = LinkedList(node)
+    link.insert(4)
+    actual = str(link)
+    expected = f'{{ 4 }} -> {{ 0 }} -> {{ 10 }} -> NULL'
+    assert actual == expected
+
+# insert a node before a node located in the middle of a linked list
+def test_insert_before():
+    node = Node(0)
+    link = LinkedList(node)
+    link.insert(4)
+    link.append(10)
+    link.insert_before(0,5)
+    actual = str(link)
+    expected = f'{{ 4 }} -> {{ 5}} -> {{ 0 }} -> {{ 10 }} -> NULL'
+    assert actual == expected
+
+
+
+
+@pytest.fixture
+def generate_new_list():
+    node = Node(0)
+    new_list = LinkedList(node)
+    list_length = 0
+
+    for value in range(1,5):
+        new_list.insert(value)
+        list_length += 1
+
+    return new_list
