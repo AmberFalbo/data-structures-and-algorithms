c07cc9f9d82aa1b0f0a03e11f72abced7db0c7c7
Amber Falbo <amberannfalbo@gmail.com>
2021-04-05 23:06:01 -0700

    some things

---
+++ python/challenges/graph/depth_first.py
@@ -0,0 +1,24 @@
+from graph.graph import Graph, Edge, Vertex
+
+class Depth_First:
+    
+    # def pre_order(self, action = None):
+    #     new_list = []
+    #     def traverse(root, action):
+    #         if not root:
+    #             return "No root to be found."
+    #         action(root.value)
+    #         # adding to list
+    #         new_list.append(root.value)
+    #         if root.left != None:
+    #             # traverse left
+    #             traverse(root.left, action)
+            
+    #         if root.right != None:
+    #             # traverse right
+    #             traverse(root.right, action)
+
+    #     traverse(self.root, action or print)
+    #     return new_list
+
+
